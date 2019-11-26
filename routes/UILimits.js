2765820114cd098d2d59ac4415fb95c26407a4a7
Mathiasami <math35i0@edu.easj.dk>
2019-11-26 14:32:01 +0100

    added the correct route to UILimits.js

--- routes/UILimits.js
+++ routes/UILimits.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET UI-Limit page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('UILimits');
   });
   
   module.exports = router;
