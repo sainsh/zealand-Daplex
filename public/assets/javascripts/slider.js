ae75d591e8c8b00ac7f7a7d884e626b2e0dd9a8e
danorlarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-11-27 13:40:12 +0100

    Fixed pug mixens

--- public/assets/javascripts/slider.js
+++ public/assets/javascripts/slider.js
@@ -7,7 +7,8 @@ var rangeBullet2 = document.getElementById("rs-bullet2");
 var rangeSlider3 = document.getElementById("rs-range-line3");
 var rangeBullet3 = document.getElementById("rs-bullet3");
 
-
+var rangeSlider4 = document.getElementById("indeSlider");
+var rangeBullet4 = document.getElementById("indeBullet");
 
 if(rangeSlider){
 rangeSlider.addEventListener("input", showSliderValue, false);}
@@ -17,6 +18,15 @@ function showSliderValue() {
   var bulletPosition = (rangeSlider.value /rangeSlider.max);
   rangeBullet.style.left = (bulletPosition * 578) + "px";
 }
+
+if(rangeSlider4){
+    rangeSlider4.addEventListener("input", showSliderValueInde, false);}
+    
+    function showSliderValueInde() {
+      rangeBullet4.innerHTML = rangeSlider4.value;
+      var bulletPosition4 = (rangeSlider4.value /rangeSlider4.max);
+      rangeBullet4.style.left = (bulletPosition4 * 578) + "px";
+    }
 
 if(rangeSlider2){
     rangeSlider2.addEventListener("input", showSliderValue2, false);}
