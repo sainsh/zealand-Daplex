dc3307170e815099285e89c5cad0fdb7eef3a4ec
madsmansour <42993941+madsmansour@users.noreply.github.com>
2019-11-27 10:35:53 +0100

    test converting csv to json

--- test/ConversionTools.test.js
+++ test/ConversionTools.test.js
@@ -2,7 +2,9 @@ const conversionTools = require('../models/ConversionTools');
 
 
 test('inputFile.xlsx gets converted to .csv file', () => {
-    expect(conversionTools.convertXlsxToCsv('./test/testfiles/Helpdesk.xlsx','./test/testfiles/bob.csv')).toBeTruthy();
+    expect(conversionTools.convertXlsxToCsv('./test/testfiles/Helpdesk.xlsx','./test/testfiles/Helpdesk.csv')).toBeTruthy();
 });
 
-
+test('convertion CSV to JSON', () => {
+    expect(conversionTools.convertCsvToJson('./test/testfiles/Helpdesk.csv')).toBeTruthy();
+});
