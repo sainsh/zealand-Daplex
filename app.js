acc49aa8a4668a36ada49dfee6a317d21860e523
danorlarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-11-27 10:20:09 +0100

    Not working

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var importRouter = require('./routes/import');
 var UILimitsRouter = require('./routes/UILimits');
+var weightUIRouter = require('./routes/weightUI');
+
 
 var app = express();
 
@@ -25,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/import', importRouter);
 app.use('/UILimits', UILimitsRouter);
+app.use('/weightUI', weightUIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
