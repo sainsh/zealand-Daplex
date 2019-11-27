73784e20a983d2de517bda55e4e651972ee33113
Mathiasami <math35i0@edu.easj.dk>
2019-11-27 14:17:54 +0100

    refactoring

--- routes/limitsUI.js
+++ routes/limitsUI.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET UI-Limit page. */
+/* GET Limit-UI page. */
 router.get('/', function(req, res, next) {
-    res.render('UILimits');
+    res.render('limitsUI');
   });
   
   module.exports = router;
