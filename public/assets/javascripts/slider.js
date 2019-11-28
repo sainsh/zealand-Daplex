d57e7cbca76f077ecc5a9f9a52946123ff1835f6
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2019-11-28 09:44:40 +0100

    Created button and grapped values from sliders

--- public/assets/javascripts/slider.js
+++ public/assets/javascripts/slider.js
@@ -1,3 +1,5 @@
+var button = document.getElementById('btn');
+
 function createSliderLogic(nameBullet, nameSlider){
 var bullet = document.getElementById(nameBullet);
 var slider = document.getElementById(nameSlider);
@@ -5,9 +7,28 @@ var slider = document.getElementById(nameSlider);
 slider.addEventListener("input", ()=>{
     bullet.innerHTML = slider.value;
     var bulletPosition = (slider.value/slider.max);
+
     bullet.style.left = (bulletPosition * 578) + "px";
 }, false);
 }
+function sendData() {
+    var indeslider = document.getElementById('indeSlider').value;
+    var udvSlider = document.getElementById('udvSlider').value;
+    var murSlider = document.getElementById('murSlider').value;
+    var tagSlider = document.getElementById('tagSlider').value;
+    var udSlider = document.getElementById('udSlider').value;
+    var tagDækSlider = document.getElementById('tagDækSlider').value;
+    var tekSlider = document.getElementById('tekSlider').value;
+    var tagrenSlider = document.getElementById('tagrenSlider').value;
+    var funSlider = document.getElementById('funSlider').value;
+    var vinSlider = document.getElementById('vinSlider').value;
+
+}
+
+btn.addEventListener("click", sendData);
+
+
+
 
 createSliderLogic("indeBullet","indeSlider");
 createSliderLogic("udvBullet","udvSlider");
