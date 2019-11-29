24adaf7bf1abd0e19ea4f8922b4c6ec0f7160062
Dalphs <42994000+Dalphs@users.noreply.github.com>
2019-11-29 12:13:31 +0100

    Created test for reading data from helpdesk_weight_data

--- test/weight.test.js
+++ test/weight.test.js
@@ -3,3 +3,9 @@ const weight = require('./weight');
 test('Returns string', () => {
     expect(weight("text")).toBe("text");
 });
+
+test('Reads from helpdesk_weight_table', () =>{
+
+
+    expect(result).toBe(mockData);
+});
