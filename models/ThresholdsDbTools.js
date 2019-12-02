2011e9090e0cd988e44bc581efba36b6c9769f7e
Cosby1992 <ande016j@edu.easj.dk>
2019-12-02 11:23:51 +0100

    Working create method for helpdesk thresholds

--- models/ThresholdsDbTools.js
+++ models/ThresholdsDbTools.js
@@ -6,13 +6,70 @@
  *  - Team Cyclone
  */
 
-exports.getHelpdeskThresholds = (sequelize, Sequelize) => {
+
+/**
+ * CRUD for thresholds table 
+ */
+ 
+createHelpdeskThreshold = async function(yellowThreshold, redThreshold, propertyId){
+
+    try{
+        let thresholdTable = getHelpdeskThresholdsTable();
+        
+
+    }
+    
+
+}
+
+async function (helpdeskWeightArray) {
+    try {
+        let helpdeskWeightTable = getHelpdeskWeightTable();
+        let resultsArray = [];
+        console.log(helpdeskWeightArray[0]);
+        let result = await helpdeskWeightTable.create({
+            property_type_id: helpdeskWeightArray[0],
+            helpdesk_indeklima: helpdeskWeightArray[1],
+            helpdesk_udv_b: helpdeskWeightArray[2],
+            helpdesk_mur_facade: helpdeskWeightArray[3],
+            helpdesk_tag: helpdeskWeightArray[4],
+            helpdesk_ud_gavl: helpdeskWeightArray[5],
+            helpdesk_tagdaekning: helpdeskWeightArray[6],
+            helpdesk_tag_ned: helpdeskWeightArray[7],
+            helpdesk_vinduer: helpdeskWeightArray[8],
+            helpdesk_fundament: helpdeskWeightArray[9],
+            helpdesk_teknisk: helpdeskWeightArray[10]
+        });
+
+
+        resultsArray.push(result.dataValues.property_type_id);
+        console.log(resultsArray[0]);
+    
+        return resultsArray; // Return an array containing all inserted IDs
+    } catch (e) {
+        throw e;
+    }
+};
+
+
+
+/** 
+ * Method returning the threshold limits table (layout).
+ * @param sequelize: from DB tools 
+ * @param Sequelize: from DB tools
+ */
+getHelpdeskThresholdsTable = (sequelize, Sequelize) => {
     return sequelize.define('helpdesk_thresholds', {
         id: {
             type: Sequelize.INTEGER,
             allowNull: false,
-            autoIncrement: true, //not sure about auto incement... - Cosby/Emil
+            autoIncrement: true, 
             primaryKey: true,
+        },
+        property_id: {
+            type: Sequelize.INTEGER,
+            refrences: 'properties',
+            refrencesKey: 'property_id'
         },
         threshold_yellow: {
             type: Sequelize.INTEGER,
@@ -23,7 +80,12 @@ exports.getHelpdeskThresholds = (sequelize, Sequelize) => {
             allowNull: false
         }
     });
-
 }
+
+
+
+
+exports.getHelpdeskThresholdsTable;
+exports.createHelpdeskThreshold;
 
 
