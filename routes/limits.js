5593d24caaf85412ba800555c7593383b0e5b5ec
Peter Clausen <peter@Peters-MacBook-Pro.local>
2019-12-02 11:32:44 +0100

    limitsFrontpage added

--- routes/limits.js
+++ routes/limits.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Limit-UI page. */
 router.get('/', function(req, res, next) {
-  res.render('limitsHelpdesk');
+  res.render('limitsFrontpage');
 });
 
 /**router.get('/', function(req, res, next){
