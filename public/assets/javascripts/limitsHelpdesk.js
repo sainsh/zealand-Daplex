97ef9355b6a6c47aee330b2554fa812d92b4b709
Peter Clausen <peter@Peters-MacBook-Pro.local>
2019-12-02 13:22:14 +0100

    Renamed limits.js Created script dummies Changed limits .pug files

---
+++ public/assets/javascripts/limitsHelpdesk.js
@@ -0,0 +1,6 @@
+var minimumValue = document.getElementById('minvalue');
+var maximumValue = document.getElementById('maxvalue');
+
+function submit(){
+    alert("Grænsen for helpdesk er sat til: " + minimumValue.value + " - " + maximumValue.value);
+}
