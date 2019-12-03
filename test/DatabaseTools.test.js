24fe52e72a36b4d7d9106cbc8792886fcacbbbbf
sainsh <kfsainsh@gmail.com>
2019-12-03 15:59:04 +0100

    added test in databaseTools.test the test inserts data into maintenanceTable

--- test/DatabaseTools.test.js
+++ test/DatabaseTools.test.js
@@ -3,7 +3,7 @@ const conversionTools = require('../models/ConversionTools')
 require('mysql2/node_modules/iconv-lite').encodingExists('foo');
 
 test('test create database',() => {
-    expect(databaseTools.setupDatabase('localhost', 'root', 'admin')).toBeTruthy();
+    expect(databaseTools.setupDatabase('localhost', 'root', 'password')).toBeTruthy();
 });
 
 test('test create database with wrong username',  () => {
@@ -22,4 +22,12 @@ test('insert data from csv', async() =>{
 test('try reading from properties where id is not a number', async()=>{
     expect(databaseTools.readProperty('to')).rejects.toThrow();
 
+})
+
+test('inserting data into maintenanceTable', async()=>{
+    let csv = await conversionTools.convertXlsxToCsv('./test/testfiles/Tilstand fra Dalux.xlsx', './test/testfiles/tilstand.csv')
+    let jsonResult = await conversionTools.convertCsvToJson('./test/testfiles/tilstand.csv');
+    let result = await databaseTools.createMaintenanceData(jsonResult);
+    let res = await databaseTools.readMaintenanceData(5);
+    await expect(res[0].dataValues).toEqual({maintenance_id: 1, property_id: 5, cost: 3250})
 })
