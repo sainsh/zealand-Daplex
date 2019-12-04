274ff6bcdb162b433934128959b0067c31cc8452
sainsh <kfsainsh@gmail.com>
2019-12-04 14:02:49 +0100

    added done statement to tests in databaseTools.test

--- test/DatabaseTools.test.js
+++ test/DatabaseTools.test.js
@@ -9,9 +9,10 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('test create database',() => {
+test('test create database',(done) => {
     
     expect(databaseTools.setupDatabase('localhost', 'root', 'password')).toBeTruthy();
+    done()
 });
 /*
 test('test create database with wrong username',  async() => {
@@ -19,27 +20,30 @@ test('test create database with wrong username',  async() => {
 });
 */
  //test works but writes files which GIT does not like
-test('insert data from csv', async() =>{
+test('insert data from csv', async(done) =>{
     await databaseTools.setupTables();
     //let csv = await conversionTools.convertXlsxToCsv('./test/testfiles/helpdesk.xlsx', './test/testfiles/helpdesk.csv')
     let jsonResult = await conversionTools.convertCsvToJson('./test/testfiles/Helpdesk.csv');
     let result = await databaseTools.createHelpdeskData(jsonResult);
     let res = await databaseTools.readProperty(1);
     await expect(res[0].dataValues).toEqual({color: null, property_id:1, property_name: 'Kildemarksvej 114 + 118 - 128', property_size: 1000, property_type_id: 420 })
+    done()
 })
 
-test('try reading from properties where id is not a number', async()=>{
+test('try reading from properties where id is not a number', async(done)=>{
    await databaseTools.setupTables();
     expect(databaseTools.readProperty('to')).rejects.toThrow();
+    done()
 
 })
 
-test('inserting data into maintenanceTable', async()=>{
+test('inserting data into maintenanceTable', async(done)=>{
     await databaseTools.setupTables()
    // let csv = await conversionTools.convertXlsxToCsv('./test/testfiles/Tilstand fra Dalux.xlsx', './test/testfiles/tilstand.csv')
     let jsonResult = await conversionTools.convertCsvToJson('./test/testfiles/tilstand.csv');
     let result = await databaseTools.createMaintenanceData(jsonResult);
     let res = await databaseTools.readMaintenanceData(5);
     await expect(res[0].dataValues).toEqual({maintenance_id: 1, property_id: 5, cost: 3250})
+    done()
 })
 
