999fe145bf814177605c1db206c5d878923a1f33
danorlarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-12-05 14:59:17 +0100

    Updated weight design

--- public/assets/javascripts/stateslider.js
+++ public/assets/javascripts/stateslider.js
@@ -22,8 +22,6 @@ select.addEventListener("change", () =>{
         console.log(sliderValues[0]);
         for (let i = 0; i < sliderValues.length; i++) {
             let value = sliderValues[i];
-            var bulletPosition = (value/sliders[i].slider.max);
-            //sliders[i].bullet.style.left = (bulletPosition * 578) + "px";
             sliders[i].slider.value = value;
             sliders[i].bullet.value = value;
             sliders[i].bullet.innerHTML = value;
